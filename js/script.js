18d06c3d369075a0e7fb6d218e16094c4d0e7017
AbogeJr <junioraboge@gmail.com>
2022-10-08 15:19:02 +0300

    mods

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const qr = document.getElementById("qrcode");
 const form = document.getElementById("form");
+const container = document.getElementById("container");
+const btn = document.getElementById("btn");
 
 var qrcode = new QRCode("qrcode");
 
@@ -11,6 +13,7 @@ form.addEventListener("submit", (e) => {
     input.focus();
   } else {
     generateCode(input.value);
+    btn ? hideBtn() : showButton();
   }
 
   console.log(input.value);
@@ -18,4 +21,17 @@ form.addEventListener("submit", (e) => {
 
 const generateCode = (text) => {
   qrcode.makeCode(text);
+};
+
+const showButton = (saveUrl) => {
+  const btn = document.createElement("button");
+  btn.innerHTML = "Download";
+  btn.classList = "btn btn-primary";
+  btn.id = "btn";
+  const saveBtn = document.getElementById("btn");
+  if (saveBtn) {
+    saveBtn.remove();
+  }
+
+  container.appendChild(btn);
 };
